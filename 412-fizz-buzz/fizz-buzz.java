376b376c2286bbae6c642e7a33bcbeeba27e7953
vaibhavsirana8474 <vaibhavsirana8474@gmail.com>
2026-09-22 15:59:27 +0530

    Time: 1 ms (99.82%) | Memory: 46.8 MB (45.76%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -5,9 +5,9 @@ class Solution {
         for(int i=1;i<=n;i++){
             if(i%3==0 && i%5==0){
                 arr.add("FizzBuzz");
-            } else if(i%3==0){
+            } else if(i%3==0 && i%5!=0){
                 arr.add("Fizz");
-            } else if(i%5==0){
+            } else if(i%5==0 && i%3!=0){
                 arr.add("Buzz");
             } else{
                 arr.add(String.valueOf(i));
