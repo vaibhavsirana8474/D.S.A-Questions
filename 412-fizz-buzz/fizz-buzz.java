46d49bb08f04c05c06fc90afbdeca2b650fbc30c
vaibhavsirana8474 <vaibhavsirana8474@gmail.com>
2026-09-22 15:56:08 +0530

    Time: 1 ms (99.82%) | Memory: 46.6 MB (83.24%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -1,5 +1,5 @@
-// import java.util.List;
-import java.util.ArrayList;
+import java.util.List;
+// import java.util.ArrayList;
 class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> arr = new ArrayList<>();
